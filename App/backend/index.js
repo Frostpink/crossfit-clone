b9c95bae70d67f1038e4b1c48429ec425cba0649
dduma032 <56765952+dduma032@users.noreply.github.com>
2021-02-10 01:23:49 -0500

    added a backend express js
    - expressjs server
    - connect server to frontend
    - connect expressjs to postgres 'crossfit' database

---
+++ App/backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const PORT = 8080;
+
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({ info: "Node.js, Express and Postgres API"})
+})
+
+app.get('/athletes', db.getAthletes)
+
+app.listen(PORT, () => {
+    console.log(`[server]: Listening on http://localhost:${PORT}/`)
+})
+
---
+++ App/backend/queries.js
@@ -0,0 +1,36 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'me',
+    host: 'localhost',
+    port: '5432',
+    database: 'crossfit',
+    password: 'password'
+})
+
+const shortid = require('shortid')
+
+// Get all athletes
+const getAthletes = (req, res) => {
+
+    let sort = 'id'
+    let get = '*'
+    if (req.query.sort) {
+        sort = req.query.sort
+    }
+    if (req.query.get) {
+        get = req.query.get
+    }
+
+    const query = `SELECT ${get} FROM athletes ORDER BY ${sort} ASC`
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+
+}
+
+module.exports = {
+    getAthletes,
+}
