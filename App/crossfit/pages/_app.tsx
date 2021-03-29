1df7a87dffe2d02f6e2502967e82e215957a51d6
dduma032 <dduma032@uottawa.ca>
2021-03-29 03:58:37 -0400

    init nextjs 2

--- App/crossfit/pages/_app.tsx
+++ App/crossfit/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+      <Head>
+        <title>Crossfit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
 }
 
 export default MyApp
